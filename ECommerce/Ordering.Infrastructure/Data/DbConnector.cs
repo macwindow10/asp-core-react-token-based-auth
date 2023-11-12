9ef5d9e92e71fab4457963a6d4f3b5d3657ea1e4
mac.window.10 <mac.window.10@gmail.com>
2023-11-12 22:44:54 +0500

    project working with sql server

---
+++ ECommerce/Ordering.Infrastructure/Data/DbConnector.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Ordering.Infrastructure.Data
+{
+    // Connection Class for Query
+    public class DbConnector
+    {
+        private readonly IConfiguration _configuration;
+
+        protected DbConnector(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            string _connectionString = _configuration.GetConnectionString("DefaultConnection");
+            //return new SqliteConnection(_connectionString);
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
